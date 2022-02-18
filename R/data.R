308716319f29a3cbc358ed277fecceae14ca901b
Brandon Alejandro Mosqueda González <brandon-mosqueda@member.fsf.org>
2022-02-17 18:11:28 -0600

    * Maize and Wheat data included in the package with docs.

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Genomic Maize data.
+#'
+#' Phenotypic and Genotype maize data.
+#'
+#' @format A list with two fields: Pheno and Geno. Pheno is a data frame with
+#' 2888 observations and 4 variables. Geno is a data frame with 722 observations
+#' of 723 variables.
+#'
+#' @source M. Bandeira e Sousa et al., "Genomic-Enabled Prediction in Maize
+#' Using Kernel Models with Genotype x Environment Interaction"
+#' \url{https://doi.org/10.1534/g3.117.042341}
+"Maize"
+
+#' Genomic Wheat data.
+#'
+#' Phenotypic and Genotype wheat data.
+#'
+#' @format A list with two fields: Pheno and Geno. Pheno is a data frame with
+#' 2396 observations and 4 variables. Geno is a data frame with 599 observations
+#' of 600 variables.
+#'
+#' @source J. Crossa et al., "Prediction of Genetic Values of Quantitative
+#' Traits in Plant Breeding Using Pedigree and Molecular Markers"
+#' \url{https://doi.org/10.1534/genetics.110.118521}
+"Wheat"
