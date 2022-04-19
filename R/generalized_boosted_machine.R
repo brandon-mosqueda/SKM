146bcb52a66e88024a06f5de12a1476cc28b5624
Brandon Alejandro Mosqueda González <brandon-mosqueda@member.fsf.org>
2022-04-19 13:57:26 -0500

    * GBM documentation examples.

--- R/generalized_boosted_machine.R
+++ R/generalized_boosted_machine.R
@@ -11,9 +11,9 @@
 #'
 #' @description
 #' `generalized_boosted_machine()` is a wrapper of the [gbm::gbm()] function
-#' with the ability to tune the hyperparameters (grid search) in a simple way.
-#' It fits univariate models for continuous, count, binary and categorical
-#' response variables.
+#' with the ability to tune the hyperparameters with grid search or bayesian
+#' optimization in a simple way. You can fit univariate models for continuous,
+#' count, binary and categorical response variables.
 #' @template tunable-description
 #'
 #' @template x-matrix-param
@@ -61,24 +61,69 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Fit with all default parameters
-#' model <- generalized_boosted_machine(to_matrix(iris[, -5]), iris$Species)
+#' # Use all default hyperparameters (no tuning) ----------------------------------
+#' x <- to_matrix(iris[, -5])
+#' y <- iris$Species
+#' model <- generalized_boosted_machine(x, y)
 #'
-#' # Tune 3 hyperparameters
+#' # Predict using the fitted model
+#' predictions <- predict(model, x)
+#' # Obtain the predicted values
+#' predictions$predicted
+#' # Obtain the predicted probabilities
+#' predictions$probabilities
+#'
+#' # Tune with grid search --------------------------------------------------------
+#' x <- to_matrix(iris[, -1])
+#' y <- iris$Sepal.Length
 #' model <- generalized_boosted_machine(
-#'   to_matrix(iris[, -1]),
-#'   iris$Sepal.Length,
+#'   x,
+#'   y,
 #'   trees_number = c(100, 200, 300),
-#'   node_size = c(5, 10),
-#'   shrinkage = c(0.001, 0.01, 0.1),
-#'   sampled_records_proportion = c(0.5, 0.75)
+#'   node_size = c(1, 2),
+#'   shrinkage = c(0.1, 0.3),
+#'   tune_type = "grid_search",
+#'   tune_cv_type = "k_fold",
+#'   tune_folds_number = 5
 #' )
 #'
-#' predictions <- predict(model, iris)
+#' # Obtain the whole grid with the loss values
+#' model$hyperparams_grid
+#' # Obtain the hyperparameters combination with the best loss value
+#' model$best_hyperparams
+#'
+#' # Predict using the fitted model
+#' predictions <- predict(model, x)
+#' # Obtain the predicted values
 #' predictions$predicted
 #'
-#' # See the whole grid
+#' # Tune with Bayesian optimization --------------------------------------------------------
+#' x <- to_matrix(iris[, -1])
+#' y <- iris$Sepal.Length
+#' model <- generalized_boosted_machine(
+#'   x,
+#'   y,
+#'   trees_number = list(min = 100, max = 500),
+#'   node_size = list(min = 1, max = 10),
+#'   tune_type = "bayesian_optimization",
+#'   tune_bayes_samples_number = 5,
+#'   tune_bayes_iterations_number = 5,
+#'   tune_cv_type = "random",
+#'   tune_folds_number = 4
+#' )
+#'
+#' # Obtain the whole grid with the loss values
 #' model$hyperparams_grid
+#' # Obtain the hyperparameters combination with the best loss value
+#' model$best_hyperparams
+#'
+#' # Predict using the fitted model
+#' predictions <- predict(model, x)
+#' # Obtain the predicted values
+#' predictions$predicted
+#'
+#' # Obtain the execution time taken to tune and fit the model
+#' model$execution_time
 #' }
 #'
 #' @export
--- man-roxygen/x-matrix-param.R
+++ man-roxygen/x-matrix-param.R
@@ -1,3 +1,3 @@
 #' @param x (`matrix`) The predictor (independet) variable(s). It must be a
 #'   numeric matrix. You can use [to_matrix()] function to convert your data to
-#'   to a `matrix`.
+#'   a `matrix`.
